2578e368057c98d23560e1baf26cbb960b169fb8
Shuvam Adhikari <shuvam.adhikar.dev@gmail.com>
2025-01-24 19:44:57 +0545

    random_Color

--- Project_6/index.js
+++ Project_6/index.js
@@ -8,3 +8,23 @@ const randomColor = function () {
   }
   return color;
 };
+
+// console.log(randomColor());
+let intervalID;
+const startChangingColor = function () {
+  if (!intervalID) {
+    intervalID = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalID);
+  intervalID = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+
+document.querySelector("#stop").addEventListener("click", stopChangingColor);

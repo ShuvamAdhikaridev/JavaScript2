e44a2e6cba67bb11c3f2284cafc40dfb4e163b9f
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-02-01 23:32:59 +0545

    Prototype

---
+++ classes_OOP/onjects.js
@@ -0,0 +1,27 @@
+function multiplyBy5(num) {
+  return num * 5;
+}
+
+multiplyBy5.power = 4;
+
+// console.log(multiplyBy5(5));
+// console.log(multiplyBy5.power);
+// console.log(multiplyBy5.prototype);
+
+function createUSer(username, score) {
+  this.username = username;
+  this.score = score;
+}
+
+createUSer.prototype.increment = function () {
+  this.score++;
+};
+
+createUSer.prototype.printME = function () {
+  console.log(`Score is ${this.score}`);
+};
+
+const Shuvam = createUSer("Shuvam", 21);
+const coffee = createUSer("Coffee", 23);
+
+Shuvam.printME();

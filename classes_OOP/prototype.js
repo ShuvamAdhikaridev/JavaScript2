f932ffa78e74a24caeef1830a64542ea6a0b715c
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-02-02 15:44:39 +0545

    Prototypes

--- classes_OOP/prototype.js
+++ classes_OOP/prototype.js
@@ -26,32 +26,45 @@ Array.prototype.helloShuvam = function () {
   console.log(`Shuvam says hello`);
 };
 
-myHero.helloShuvam();
-// heroPower.helloShuvam();
+// myHero.helloShuvam();
+// // heroPower.helloShuvam();
 
-const user = {
-  name: "Shuvam",
-  age: 11,
+// const user = {
+//   name: "Shuvam",
+//   age: 11,
+// };
+
+// const Teacher = {
+//   makeVideo: true,
+// };
+
+// const teachingSupport = {
+//   isAvailable: false,
+// };
+
+// const taSupport = {
+//   makeAssignment: "JS assignment",
+//   fullTime: true,
+//   __proto__: teachingSupport,
+// };
+
+// console.log(taSupport);
+
+// Teacher.__proto__ = user;
+
+// console.log(Teacher);
+
+// Object.setPrototypeOf(teachingSupport, Teacher);
+
+let anotherUSerNAme = "Shuvam     ";
+
+String.prototype.trueLength = function () {
+  console.log(this);
+  console.log(`${this.name}`);
+  console.log(`True Length is : ${this.trim().length}`);
 };
 
-const Teacher = {
-  makeVideo: true,
-};
+anotherUSerNAme.trueLength();
 
-const teachingSupport = {
-  isAvailable: false,
-};
-
-const taSupport = {
-  makeAssignment: "JS assignment",
-  fullTime: true,
-  __proto__: teachingSupport,
-};
-
-console.log(taSupport);
-
-Teacher.__proto__ = user;
-
-console.log(Teacher);
-
-Object.setPrototypeOf(teachingSupport, Teacher);
+"Hello".trueLength();
+"Coffe".trueLength();

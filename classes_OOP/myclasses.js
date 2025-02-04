7024637c1409b4ac141bb69c8a7db85c073c490f
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-02-04 20:05:51 +0545

    Constructor_slight_Changes

--- classes_OOP/myclasses.js
+++ classes_OOP/myclasses.js
@@ -10,8 +10,12 @@ class User {
   encryptPAssword() {
     return `${this.password}abc`;
   }
+  changeUserName() {
+    return `${this.username.toLowerCase()}`;
+  }
 }
 
 const hello = new User("Shuvam", "Shuvamlim234@gmail.com", 12311);
 
 console.log(hello.encryptPAssword());
+console.log(hello.changeUserName());

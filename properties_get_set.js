b1d377d2afb21c8591e23504f6d58b033434ca73
Shuvam Adhikari <shuvam.adhikari.dev@gmail.com>
2025-02-06 19:32:04 +0545

    Properties_Get_Set

---
+++ properties_get_set.js
@@ -0,0 +1,27 @@
+function User(email, password) {
+  this._email = email;
+  this._password = password;
+
+  Object.defineProperty(this, "email", {
+    get: function () {
+      return this._email.toUpperCase();
+    },
+    set: function (value) {
+      this._email = value;
+    },
+  });
+
+  Object.defineProperty(this, "password", {
+    get: function () {
+      return this._password.toUpperCase();
+    },
+    set: function (value) {
+      this._password = value;
+    },
+  });
+}
+
+const shuvam = new User("Hello11@gmail.com", "chai");
+
+console.log(shuvam);
+console.log(shuvam.email);
